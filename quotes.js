4ffc5429e57c1744749ecdb75d5e903253fd29ed
juliamirosh-gif <guliamirosh@gmail.com>
2026-07-07 19:01:25 +0300

    добавила модули

---
+++ quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+  {
+    quote:
+      "It is only with the heart that one can see rightly; what is essential is invisible to the eye.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote: "You become responsible, forever, for what you have tamed.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote:
+      "All grown-ups were once children... but only few of them remember it.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote: "The stars are beautiful because of a flower that cannot be seen.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote:
+      "The most beautiful things in the world cannot be seen or touched, they are felt with the heart.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote: "What makes the desert beautiful is that somewhere it hides a well.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote:
+      "To me, you are still nothing more than a little boy just like a hundred thousand other little boys.",
+    author: "Antoine de Saint-Exupéry",
+  },
+
+  {
+    quote: "One sees clearly only with the heart.",
+    author: "Antoine de Saint-Exupéry",
+  },
+];
+
+export default quotes;
